0b7ccaf5fd1241345015a02eea417fccc1c60d1c
barteaga <barteaga@umich.edu>
2020-06-04 14:20:49 -0400

    Save message and load message functions to DataService

--- Simple-Chat/Services/DataService.swift
+++ Simple-Chat/Services/DataService.swift
@@ -17,6 +17,37 @@ class DataService {
     static let instance = DataService()
     let ref = Firebase.Database.database().reference()
     var messages: [Message] = []
-    weak var delegat: DataServiceDelegate?
+    weak var delegate: DataServiceDelegate?
+    
+    //loads messages from Firebase and stores the in messages array
+    func loadMessages(_ completion: @escaping (_ Success: Bool) -> Void) {
+        //observes the value of that Firebase location
+        ref.observe(.value) { (data: Firebase.DataSnapshot) in
+            if data.value != nil {
+                let unsortedMessages = Message.messageArrayFromFBData(data.value! as AnyObject)
+                //sorts messages into chronological order
+                self.messages = unsortedMessages.sorted(by: {$0.messageId < $1.messageId})
+                self.delegate?.dataLoaded()
+                if self.messages.count > 0 {
+                    completion(true)
+                }
+                else {
+                    completion(false)
+                }
+            }
+            else {
+                completion(false)
+            }
+        }
+    }
+    
+    //function to save a message to our Firebase backend
+    func saveMessage(_ user: String, message: String) {
+        //autoId has a timestamp in it so that we can later sort the messages in chronological order
+        let key = ref.childByAutoId().key
+        let message = ["user": user, "message": message]
+        let messageUpdates = ["/\(key)": message]
+        ref.updateChildValues(messageUpdates)
+    }
 }
 
