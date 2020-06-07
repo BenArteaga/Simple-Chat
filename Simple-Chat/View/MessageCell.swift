07022a583f23aef53e69df961ba4a17bfc34ac3a
barteaga <barteaga@umich.edu>
2020-06-07 19:03:13 -0400

    MessageCell class complete

---
+++ Simple-Chat/View/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Simple-Chat
+//
+//  Created by Ben Arteaga on 6/7/20.
+//  Copyright © 2020 Ben Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configureCell(user: String, message: String) {
+        userLabel.text = user
+        messageLabel.text = message
+    }
+
+}
